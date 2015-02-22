5cc5472cffa6a4bbc51742f52545eaeb680a05d5
Akshay Mehra <akshay.m.1711@gmail.com>
2015-02-21 23:01:28 -0500

    User signup/login, skeleton done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
+
   get 'signup' => 'users#new'
 
   root 'staticpages#home'
