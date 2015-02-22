07ef62ff74a95bab8c7896f23b01126ee3f76eae
Akshay Mehra <akshay.m.1711@gmail.com>
2015-02-21 22:17:17 -0500

    finish signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   
+  get 'signup' => 'users#new'
+
   root 'staticpages#home'
 
   get 'about' => 'staticpages#about'
 
   get 'contact' => 'staticpages#contact'
+
+  resources :users
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222011602) do
+ActiveRecord::Schema.define(version: 20150222025528) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255
@@ -21,5 +21,7 @@ ActiveRecord::Schema.define(version: 20150222011602) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
