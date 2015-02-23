bba825f466167764dd5871838962f5b47174437c
cloudyshawn <shawn.krivorot@hotmail.com>
2015-02-23 08:55:18 -0500

    added validation on posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+	belongs_to :user
+	
+	validates :title, presence: true
+	validates :address, presence: true
+	validates :postedby, presence: true
+	validates :description, presence: true
+	validates :city, presence: true
+	validates :country, presence: true
+	validates :size, presence: true
 end
